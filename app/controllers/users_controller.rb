3183c053b122a7ce637a379290197ab11dc20737
聡志伊藤 <i.satoshi.0915@gmail.com>
2020-08-09 02:12:53 +0900

    アラートの出ているユーザー一覧取得を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   # TODO: 将来対応
-  # クエリパラメーター: userName, address, disclose_id
+  # クエリパラメーター: userName, address, disclose_id, is_alert
   def index
-    @users = User.all.map(&:to_index_hash)
+    if params["is_alert"]
+      answer_ids = AnserOption.
+                   where(is_alert: true).
+                   answer_contents.
+                   pluck(:answer_id)
+      @users = User.where(id: Answer.where(id: answer_ids).pluck(:user_id)).
+                    map(&:to_index_hash)
+    else
+      @users = User.all.map(&:to_index_hash)
+    end
   end
 
   def show
