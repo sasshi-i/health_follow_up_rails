0456b992af2a930ce2af64d5d5649e0d96160f71
聡志伊藤 <i.satoshi.0915@gmail.com>
2020-09-01 21:50:50 +0900

    jwt tokenをcookieにセットするように修正

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,3 +1,13 @@
 class UserTokenController < Knock::AuthTokenController
   protect_from_forgery :except => [:create]
+
+  def create
+    cookies[:jwt] = { 
+                      value: auth_token.token,
+                      expires: 1.day.from_now,
+                      same_site: "None",
+                      secure: true
+                    }
+    super
+  end
 end
